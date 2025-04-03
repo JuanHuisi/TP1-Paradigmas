08f976fd710b971d043f65eca7fa317da95f2d66
Tom <hernandeztomas584@gmail.com>
2025-04-03 18:52:04 -0300

    add: uwu

--- tp1-base/src/PPON.hs
+++ tp1-base/src/PPON.hs
@@ -34,4 +34,18 @@ aplanar :: Doc -> Doc
 aplanar = foldDoc vacio (\s rec -> texto s <+> rec) (\n rec -> texto " " <+> rec)
 
 pponADoc :: PPON -> Doc
-pponADoc = error "PENDIENTE: Ejercicio 9"
+pponADoc (TextoPP s) = texto (show s)
+pponADoc (IntPP n) = texto (show n)
+pponADoc (ObjetoPP l) = if(pponObjetoSimple (ObjetoPP l)) 
+                        then texto "{ " <+> intercalar (texto ", ") rec <+> texto " }"  
+                        else entreLlaves $ rec
+  where rec = map(\(x, y) -> texto (show x) <+> texto ": " <+> pponADoc y) l
+
+{- data PPON
+  = TextoPP String
+  | IntPP Int
+  | ObjetoPP [(String, PPON)]
+  deriving (Eq, Show)
+
+  f x : rec
+ -}
