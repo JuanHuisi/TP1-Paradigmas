fd0f7e500991b2f17ff33b6f1f107d6ff29798d5
1sgnslc <114841448+1sgnslc@users.noreply.github.com>
2025-04-10 14:54:36 -0300

    Update Main.hs
    agrego comentarios sobre tests del 1, 5, agrego tests del 2, 3, 4

--- tp1-base/src/Main.hs
+++ tp1-base/src/Main.hs
@@ -19,36 +19,58 @@ allTests =
       "Ejercicio 9" ~: testsEj9
     ]
 
+-- obs : no agrego test del 1 porque el fold se catchea en los tests del 2 al 100%
 testsEj2 :: Test
 testsEj2 =
   test
-    [ vacio <+> vacio ~?= vacio,
-      texto "a" <+> texto "b" ~?= texto "ab",
-      (texto "a" <+> linea) <+> texto "b" ~?= texto "a" <+> (linea <+> texto "b")
+    [ vacio <+> vacio ~?= vacio,                                                    -- testea vacio - vacio
+      texto "a" <+> texto "b" ~?= texto "ab",                                       -- testea (texto vacio) - (texto vacio)
+      (texto "a" <+> linea) <+> texto "b" ~?= texto "a" <+> (linea <+> texto "b"),  -- testea (texto linea) - (texto vacio)
+      vacio <+> texto "a" ~?= texto "a",                                            -- testea vacio - (texto vacio)
+      vacio <+> linea ~?= linea,                                                    -- testea vacio (linea vacio)
+      linea <+> vacio ~?= linea,                                                    -- testea (linea vacio) vacio   
+      -- puse estos 2 q "hacen lo mismo" pero el fold debería catchearlo de formas distintas
+      (linea <+> texto "b") <+> linea ~?= linea <+> (texto "b" <+> linea)           -- ult test de linea
     ]
+    -- un par para probar en ghci (deberían ser True (?))
+    -- texto "a" <+> linea == Texto "a" (Linea 0 Vacio)        -- testea (texto linea) - (linea vacio
+    -- linea <+> linea == Linea 0 (Linea 0 Vacio)              -- testea (linea vacio) - (linea vacio)
+    -- debería ser lo suficientemente exhaustivo para considerar todos los casos sumandole los tests del ej8 y ej9.
 
 testsEj3 :: Test
 testsEj3 =
   test
-    [ indentar 2 vacio ~?= vacio,
-      indentar 2 (texto "a") ~?= texto "a",
+    [ indentar 2 vacio ~?= vacio,           -- test vacio
+      indentar 2 (texto "a") ~?= texto "a", -- test sin lineas 
       indentar 2 (texto "a" <+> linea <+> texto "b") ~?= texto "a" <+> indentar 2 (linea <+> texto "b"),
-      indentar 2 (linea <+> texto "a") ~?= indentar 1 (indentar 1 (linea <+> texto "a"))
+      indentar 2 (linea <+> texto "a") ~?= indentar 1 (indentar 1 (linea <+> texto "a")), -- test linea se suma bien
+      indentar 0 (linea <+> texto "a") ~?= linea <+> texto "a" -- test trivial
+      
     ]
+{- Para ver sobre casos de test del ej3, especificamente como se handlean las lineas leer la sección comentada en indentar' dentro de Documentar.hs-}
 
 testsEj4 :: Test
 testsEj4 =
   test
     [ mostrar vacio ~?= "",
       mostrar linea ~?= "\n",
-      mostrar (indentar 2 (texto "a" <+> linea <+> texto "b")) ~?= "a\n  b"
+      mostrar (indentar 2 (texto "a" <+> linea <+> texto "b")) ~?= "a\n  b",
+
+      -- agrego tests (los q están en el enunciado)
+      mostrar (texto "abc") ~?= "abc",
+      mostrar (texto "a" <+> texto "b") ~?= "ab",
+      mostrar (indentar 2 (texto "abc" <+> linea <+> texto "def")) ~?= "abc\n  def",
+      mostrar (indentar 2 (texto "a" <+> linea <+> texto "b" <+> linea <+> texto "c")) ~?= "a\n  b\n  c"
     ]
+
 
 pericles, merlina, addams, familias :: PPON
 pericles = ObjetoPP [("nombre", TextoPP "Pericles"), ("edad", IntPP 30)]
 merlina = ObjetoPP [("nombre", TextoPP "Merlina"), ("edad", IntPP 24)]
 addams = ObjetoPP [("0", pericles), ("1", merlina)]
 familias = ObjetoPP [("Addams", addams)]
+
+-- no agrego tests del ej5 porque los catchea los tests del ej9
 
 testsEj6 :: Test
 testsEj6 =
