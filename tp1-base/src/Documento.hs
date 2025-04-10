4f268e0f1521b0babfc27fbdb33861c39f3d5aaa
1sgnslc <114841448+1sgnslc@users.noreply.github.com>
2025-04-10 13:57:12 -0300

    Update Documento.hs
    fix ej3 (indentar) + comments

--- tp1-base/src/Documento.hs
+++ tp1-base/src/Documento.hs
@@ -52,9 +52,40 @@ infixr 6 <+>
                       (\n rec -> if rec == Vacio then Linea n d2 else  Linea n rec)
                       d1 
  
-{- TODO: Se estan agregando las lineas en todas. Habria que exceptuar la primera -}
-indentar :: Int -> Doc -> Doc
-indentar i = foldDoc Vacio (\s rec -> Texto s rec) (\n rec -> Linea (n+i) rec) 
+-- esto no resuelve bien, está agregando en todas incluso la primera
+indentar' :: Int -> Doc -> Doc 
+indentar' i  = foldDoc (Vacio) (\s res -> Texto s res) (\n res -> Linea (n + i) res)
+
+{- Primero se consideró usar recursión primitiva con recr para tener guardada la estructura
+original del documento. Pero al pensar un poco una resolución nos dimos cuenta que no sería necesario
+tener lo original.-}
+
+recrDoc :: b -> (String -> Doc -> b -> b) -> (Int -> Doc -> b -> b) -> Doc -> b 
+recrDoc fVacio fTexto fLinea doc = case doc of 
+                                  Vacio -> fVacio 
+                                  Texto s d -> fTexto s d (rec d)
+                                  Linea i d -> fLinea i d (rec d)
+                                  where rec = recrDoc fVacio fTexto fLinea
+
+{- 
+La idea que consideramos para resolver esto es un approach usado en problemas recursivos, que se aplica 
+en backtracking, divide and conquer, o cualquier tipo de problema con algoritmos recursivos.
+La idea es usar a la función indentar como wrapper de una función auxiliar que mantiene una estructura de datos diferente. 
+En este caso, la recursión mantiene (devuelve) una tupla (Doc, Bool) tal que el valor booleano arranca en True pero tras hacer 1 llamado recursivo pasa a False
+y no varía más, así considerando el caso borde de la primera iteración.
+
+Obs -> Si tengo documento que arranca en linea,
+x ej: indentar 2 (Linea 1 (texto "a")), debería devolver Linea 1 (texto "a")
+-}
+
+indentar :: Int -> Doc -> Doc 
+indentar i doc = fst (indentarAux i doc True)
+
+indentarAux :: Int -> Doc -> Bool -> (Doc, Bool)
+indentarAux i doc esPl = foldDoc (Vacio, False) 
+                        (\s (resd, primeraLinea) -> (Texto s resd, False))
+                        (\n (resd, primeraLinea) -> if primeraLinea then (Linea n resd, False)
+                                                                   else (Linea (n + i) resd, False)) doc 
 
 mostrar :: Doc -> String
 mostrar = foldDoc "" (\s rec -> s ++ rec) (\n rec -> "\n" ++ take n (repeat ' ') ++ rec) 
