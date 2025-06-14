553c6f74c6a35478cd4b1526a4f90e9de74df863
Tom <hernandeztomas584@gmail.com>
2025-06-14 17:38:42 -0300

    add: reversibilidad

--- TP 2/katamino.pl
+++ TP 2/katamino.pl
@@ -1,12 +1,23 @@
 :- use_module(piezas).
 
 % EJ 1
+/*
+    Justificación de Reversibilidad
+    Para que el predicado sea reversible debe suceder que:
+        1. No se cuelga.
+        2. Debe contener al resultado que retorna el programa cuando el argumento tiene la instanciación contraria.
+    Para eso, podemos chequear cada uno de los argumentos por separado para ver qué le sucede al predicado.
+    
+    Reversibilidad en el primer argumento (-Descartar): No es reversible en nuestra implementación. Esto es debido a que length(X, Descartar) genera todas las posibles listas X de longitud Descartar y como ambas variables no están instanciadas generará infinitas posibles unificaciones que luego tratará de unificar con el append(X, Y, L) lo cual nunca valdrá una vez que length(X) > length(L) y Descartar > length(L).
+    Ej.: append([_, _, _], y, [a, b]) no tiene sentido, lo cual append([_, _, _, _], y, [a, b]) menos aún pues la cantidad de elementos de la lista original es 2 y no hay forma de generar una sublista de 3 elementos. Entonces, como no generamos una cota máxima para el valor de X y Descartar tiende a generar soluciones aparentemente infinitas, pero que ninguna satisface el último append.
+
+    Reversibilidad en el cuarto argumento (+R): Es reversible en nuestra implementación. Como sublista en su firma de la función tiene (+Descartar, +Tomar, +L, -R) si instanciamos R la respuesta del predicado será un true / false depende de si R unifica con alguna de las sublistas generadas por el predicado para las condiciones de Descartar, Tomar y L en particular.
+    R es utilizado tanto en length y append, ambos predicados reversibles. Lo cual, si lo enviamos o no, funcionará como esperamos, siempre y cuando cumplamos con la firma de los demás parámetros evitando recursiones infinitas.
+
+    Reversibilidad en el primer y cuarto argumento(-Descartar, +R): Por el mismo argumento que utilizamos en la justificación del primer argumento (Descartar), por más que instanciemos R va a seguir fallando (se cuelga) puesto que si no instanciamos Descartar siempre llegará el momento en que buscará la forma de generar listas de mayor longitud que la que le proporcionamos en length(X, Descartar), lo cual, jamás unificará con ninguno de los append.
+*/
 % sublista(+Descartar, +Tomar, +L, -R)
-sublista(Descartar, Tomar, L, R) :-
-    length(R, Tomar),
-    length(X, Descartar),
-    append(X, Y, L),
-    append(R, _, Y).
+sublista(Descartar, Tomar, L, R) :- length(R, Tomar), length(X, Descartar), append(X, Y, L), append(R, _, Y).
 
 % EJ 2
 % tablero(+K, -T)
